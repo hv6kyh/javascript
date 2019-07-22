2410e0ea6268512b213280497675883d75d82720
hv6kyh <hv6kyh@naver.com>
2019-07-22 10:10:49 +0900

    js

---
+++ demo1.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+// CDN 방식으로 로드
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script>
+    jQuery( document ).ready(function( $ ) {
+      $('body').prepend('<h1>Hello world</h1>');
+    });
+    </script>
+</body>
+</html>
